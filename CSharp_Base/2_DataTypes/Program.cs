11f640d3208f63b5be7ad7151af93e34f647d3a0
Alexander <alexandersviridenko98@gmail.com>
2020-01-20 00:09:29 +0200

    add solution file

---
+++ CSharp_Base/2_DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
