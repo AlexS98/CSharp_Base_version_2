54f8d099604edb679b175055735bf1cea1dc072d
Alexander <alexandersviridenko98@gmail.com>
2020-01-22 20:34:23 +0200

    lesson 3 p1

---
+++ CSharp_Base/3_Statements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _3_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correctOperator = true;
+            do
+            {
+                Console.WriteLine("Enter operation");
+                string v = Console.ReadLine();
+                correctOperator = true;
+                switch (v)
+                {
+                    case "+":
+                        Console.WriteLine($"Find {v}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"Find {v}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"Find {v}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"Find {v}");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong operation!");
+                        correctOperator = false;
+                        break;
+                }
+            }
+            while (!correctOperator);
+        }
+    }
+}
