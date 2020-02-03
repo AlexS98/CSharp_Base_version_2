1c2e78bf2d58f6a45571baa8da66f53d0d2b321d
Alexander <alexandersviridenko98@gmail.com>
2020-02-03 22:20:49 +0200

    lesson 5

---
+++ CSharp_Base/5_Methods/Lesson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5_Methods
+{
+    internal static class Lesson
+    {
+        public static void Task1()
+        {
+            do
+            {
+                int result = 0;
+                do
+                {
+                    Console.WriteLine("Please, enter a size: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out result) || result <= 0);
+
+                int[,] array = new int[result, result];
+
+                for (int i = 0; i < result; i++)
+                {
+                    for (int k = 0; k < result; k++)
+                    {
+                        array[i, k] = k <= i ? (i + 1) * (k + 1) : 0;
+                        Console.Write("{0};\t", array[i, k]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Press y + [Enter] to continue...");
+            }
+            while (Console.ReadLine().Equals("y", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ CSharp_Base/5_Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _5_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lesson.Task1();
+
+            int size = 0;
+            do
+            {
+                size = ReadInteger("Enter size: ");
+            }
+            while (size <= 0);
+
+            int[,] array = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    array[i, k] = (i + k) % 2 == 1 ? (i + 1) * (k + 1) : 0;
+                }
+            }
+
+            ShowArray(array);
+
+        }
+
+        static void ShowArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int k = 0; k < array.GetLength(1); k++)
+                {
+                    Console.Write("{0}\t", array[i, k]);
+                }
+            }
+        }
+
+        static int ReadInteger(string q)
+        {
+            int number = 0;
+            bool @continue;
+            do
+            {
+                Console.WriteLine(q);
+                string input = Console.ReadLine();
+                @continue = !int.TryParse(input, out number);
+            }
+            while (@continue);
+            return number;
+        }
+    }
+}
