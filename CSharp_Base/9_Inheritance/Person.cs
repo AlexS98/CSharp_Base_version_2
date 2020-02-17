bca0d5c967f301639de0a4f388122d1e538cfe53
Alexander <alexandersviridenko98@gmail.com>
2020-02-17 21:40:51 +0200

    lesson

---
+++ CSharp_Base/9_Inheritance/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9_Inheritance
+{
+    class Person
+    {
+    }
+}
---
+++ CSharp_Base/9_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
